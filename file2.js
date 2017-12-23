41e55849f21108e8ae0a24e82d94f34a55936697
Zihe Huang <zihehuang@gmail.com>
2017-12-23 10:37:48 -0800

    initial commits

---
+++ file2.js
@@ -0,0 +1,18 @@
+function emailActiveClients(clients) {
+  clients
+    .filter(isActiveClient)
+    .forEach(email);
+}
+
+function isActiveClient(client) {
+  const clientRecord = database.lookup(client);
+  return clientRecord.isActive();
+}
+
+function parseBetterJSAlternative(code) {
+  const tokens = tokenize(code);
+  const ast = lexer(tokens);
+  ast.forEach((node) => {
+    // parse...
+  });
+}
